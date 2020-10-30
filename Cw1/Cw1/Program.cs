982fe0deaf3bc67ad32ad273c4420cbb225b2ed2
s16537 <s16537@pjwstk.edu.pl>
2020-10-30 14:33:39 +0100

    first commit

---
+++ Cw1/Cw1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Cw1
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(args[0]);
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                var contents = await response.Content.ReadAsStringAsync();
+
+                Regex emailRegex = new Regex(@"\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*", RegexOptions.IgnoreCase);
+                MatchCollection emailMatches = emailRegex.Matches(contents);
+
+                foreach (Match email in emailMatches)
+                {
+                    Console.WriteLine(email.Value);
+                }
+            }
+        }
+    }
+}
