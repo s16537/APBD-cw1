4e1f6669f9a4f1decf14256ec0385b21fbbe818e
s16537 <s16537@pjwstk.edu.pl>
2020-10-30 15:11:55 +0100

    aplikacja wymagan

--- Cw1/Cw1/Program.cs
+++ Cw1/Cw1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Text.RegularExpressions;
@@ -11,7 +12,20 @@ namespace Cw1
         public static async Task Main(string[] args)
         {
             var httpClient = new HttpClient();
+
+            if (args.Length == 0)
+            {
+                throw new ArgumentNullException();
+            }
+            else if (!Uri.IsWellFormedUriString(args[0], UriKind.Absolute))
+            {
+                throw new ArgumentException("Niepoprawny adres podany jako argument.");
+            }
+
             var response = await httpClient.GetAsync(args[0]);
+
+            httpClient.Dispose();
+            List<string> addresses = new List<string>();
 
             if (response.StatusCode == HttpStatusCode.OK)
             {
@@ -22,8 +36,29 @@ namespace Cw1
 
                 foreach (Match email in emailMatches)
                 {
-                    Console.WriteLine(email.Value);
+                    //Console.WriteLine(email.Value);
+                    var mailData = email.Value;
+                    if (!addresses.Contains(mailData))
+                    {
+                        addresses.Add(email.Value);
+                    }
                 }
+
+                if(addresses.Count == 0)
+                {
+                    Console.WriteLine("Nie znaleziono adresow email.");
+                }
+                else
+                {
+                    foreach(string address in addresses)
+                    {
+                        Console.WriteLine(address);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Blad w czasie pobierania strony.");
             }
         }
     }
